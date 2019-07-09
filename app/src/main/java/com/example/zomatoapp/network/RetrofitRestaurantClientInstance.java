046951e8a0eb1d07d5ae988ab8e0857bb5d0570a
Your Name <you@example.com>
2019-07-09 15:12:14 +0530

    added retrofin Instance class

---
+++ app/src/main/java/com/example/zomatoapp/network/RetrofitRestaurantClientInstance.java
@@ -0,0 +1,17 @@
+package com.example.zomatoapp.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitRestaurantClientInstance {
+        private static Retrofit retrofit;
+        private static final String BASE_URL = "https://developers.zomato.com/api/v2.1";
+
+        public static Retrofit getRestaurantRetrofitInstance() {
+            if (retrofit == null) {
+                retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create()).build();
+            }
+            return retrofit;
+        }
+}
